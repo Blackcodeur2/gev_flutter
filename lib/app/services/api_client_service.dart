a182ededc833deb413d99f13e482750b38be4818
blackcodeur237 <shadowhunterng2@gmail.com>
2026-05-15 12:01:30 +0100

    ok

--- lib/app/services/api_client_service.dart
+++ lib/app/services/api_client_service.dart
@@ -26,37 +26,9 @@ class ApiClient {
       return _instance;
     }
 
-    final baseUrl = await _instance._findAvailableBaseUrl();
-    _instance._configureDio(baseUrl);
+    _instance._configureDio(AppConstants.apiBaseUrl);
     _initialized = true;
     return _instance;
-  }
-
-  Future<String> _findAvailableBaseUrl() async {
-    for (final url in AppConstants.apiBaseUrls) {
-      try {
-        final testDio = Dio(
-          BaseOptions(
-            baseUrl: url,
-            connectTimeout: AppConstants.apiTimeOut,
-            receiveTimeout: AppConstants.apiTimeOut,
-          ),
-        );
-
-        await testDio.get(
-          '',
-          options: Options(validateStatus: (_) => true),
-        );
-
-        print('ApiClient: base URL disponible -> $url');
-        return url;
-      } catch (_) {
-        print('ApiClient: échec de connexion à $url');
-      }
-    }
-
-    print('ApiClient: aucune base URL disponible, utilisation de la valeur par défaut.');
-    return AppConstants.apiBaseUrl;
   }
 
   void _configureDio(String baseUrl) {
